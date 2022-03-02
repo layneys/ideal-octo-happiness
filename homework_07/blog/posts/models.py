98a3426897558840d6b1f25383764e8f4c059d8e
layney121 <coolan151103@yandex.ru>
2022-03-02 14:24:13 +0300

    Homeworks 5,7,8 retry

---
+++ homework_07/blog/posts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Post(models.Model):
+    preview_pic = models.ImageField(blank=True, upload_to="post_preview_pics")
+    title = models.CharField(max_length=64)
+    body = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'пост'
+        verbose_name_plural = 'посты'
+        ordering = ['pk']
+
